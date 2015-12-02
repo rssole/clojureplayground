c5cabb21b756cf1798e0bd8fc3e8b53510115935
Rastko Soskic <rastko.soskic@gtech.com>
2015-12-02 11:46:52 +0100

    4Clojure problem 78

--- src/fourclojure/4_clojure.clj
+++ src/fourclojure/4_clojure.clj
@@ -1,6 +1,6 @@
 (ns fourclojure.4-clojure)
 
-;problem 69
+;problem 69 merge with function
 ((fn [f & ms]
    (into {} (for [[k v]
                   (group-by first
@@ -8,14 +8,14 @@
                               [k v]))]
               {k (reduce f (map second v))}))) concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})
 
-;problem 70
+;problem 70 camel casing
 ((fn [s]
    (let [[a & others] (clojure.string/split s (re-pattern "-"))]
      (apply str a
             (map #(let [[x & ys] %]
                     (apply str (Character/toUpperCase x) ys)) others)))) "multi-word-key")
 
-;problem 75
+;problem Euler's totient function
 ((fn [x] (letfn [(gcd [a b]
                    (loop [ia a ib b]
                      (if (= ib 0)
@@ -23,7 +23,7 @@
                        (recur ib (mod ia ib)))))]
            (count (filter #(= 1 (gcd % x)) (range 1 (inc x)))))) 99)
 
-;problem 86
+;problem 86 happy numbers
 ((fn [x]
    (letfn [(square-ch [y] (let [n (Integer/parseInt (str y))] (* n n)))]
      (loop [s (str x) ctrl #{}]
@@ -33,3 +33,13 @@
            (= s2 1) true
            (ctrl s2) false
            :else (recur (str s2) (conj ctrl s2))))))) 7)
+
+;problem 78 reimplement trampoline
+(letfn [(triple [x] #(sub-two (* 3 x)))
+        (sub-two [x] #(stop?(- x 2)))
+        (stop? [x] (if (> x 50) x #(triple x)))]
+  ((fn my-trampo [f & args]
+     (loop [g (apply f args)]
+       (if (ifn? g)
+         (recur (g))
+         g))) triple 2))
