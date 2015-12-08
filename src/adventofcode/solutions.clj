fdcbd780a3acdfe8d169e840cb13319bd2c3cce9
Rastko Soskic <rastko.soskic@gtech.com>
2015-12-08 16:43:48 +0100

    adventofcode - day 5

--- src/adventofcode/solutions.clj
+++ src/adventofcode/solutions.clj
@@ -1,5 +1,6 @@
 (ns adventofcode.solutions
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:import (java.security MessageDigest)))
 
 
 (defn day-x-input [x] (str "adventofcode/day" x "-input"))
@@ -33,10 +34,10 @@
 
 ; day 3
 (def -move #(case %
-            \^ [%2 (inc %3)]
-            \v [%2 (dec %3)]
-            \< [(dec %2) %3]
-            \> [(inc %2) %3]))
+             \^ [%2 (inc %3)]
+             \v [%2 (dec %3)]
+             \< [(dec %2) %3]
+             \> [(inc %2) %3]))
 
 (defn day3 []
   (let [input (slurp-day-input 3)]
@@ -56,3 +57,46 @@
                   [[nval m-nval] (conj s nval m-nval)])
                 [[[0 0] [0 0]] #{[0 0]}]
                 input)))))
+
+; day 4
+(defn md5 [s]
+  (let [algorithm (MessageDigest/getInstance "MD5")
+        size (* 2 (.getDigestLength algorithm))
+        raw (.digest algorithm (.getBytes s))
+        sig (.toString (BigInteger. 1 raw) 16)
+        padding (apply str (repeat (- size (count sig)) "0"))]
+    (str padding sig)))
+
+(fn day-4-both-parts []
+  (let [input "iwrupvqb"]
+    (reduce #(let [m5 (md5 (str input %2))]
+              (if (.startsWith m5 "00000")                  ;part 2 just asks for one more zero :)
+                (reduced %2)
+                %2))
+            (range))))
+
+; day 5
+(defn day5 []
+  (let [input (day-input-line-seq 5)]
+    (count
+      (reduce (fn [a i] (let [vcnt (count (filter #(#{\a \e \i \o \u} %) i))
+                              has-dbl (some #(apply = %) (partition 2 1 i))
+                              allowed (not (some true? (map #(.contains i %) ["ab", "cd", "pq", "xy"])))]
+                          (if (and has-dbl (>= vcnt 3) allowed)
+                            (conj a i)
+                            a)))
+              []
+              input))))
+
+; day 5 - part 2
+(defn day5-part2 []
+  (let [input (day-input-line-seq 5)]
+    (count
+      (reduce (fn [a i] (let [has2pairs (re-find #"((\w)(\w)).*(\2\3)" i)
+                              rletter-w-1-between (re-find #"(\w)\w\1" i)]
+                          (if (and has2pairs rletter-w-1-between)
+                            (conj a i)
+                            a)))
+              []
+              input))))
+
