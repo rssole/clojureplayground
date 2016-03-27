3fd736262c6c428f5ce1041e3e9f2fc7860b8aad
rastko <rssole@gmail.com>
2016-03-27 17:51:35 +0200

    Homomorphism

--- test/clojureplayground/monads_learn_test.clj
+++ test/clojureplayground/monads_learn_test.clj
@@ -36,10 +36,41 @@
 
     ;Again, applicative functors must obey to the particular laws
 
+    ;Identity
     ;Feeding a function f to pure and applying the resulting
     ; Applicative to the Functor v should be the same as
     ; directly mapping f over the Functor v
 
     ;Haskell way: pure f <*> v = fmap f v
     (testing "Identity"
-      (is (= (<*> (pure List inc) xs) (fmap xs inc))))))
+      (is (= (<*> (pure List inc) xs) (fmap xs inc))))
+
+    ;Composition
+    ;The result of applying an Applicative Functor
+    ; that yields the function composition operator to the Applicative u,
+    ; then apply the resulting Functor to v and finally applying that result
+    ; to the final Applicative w should be the same as applying v to w and
+    ; then applying u to the resulting Applicative.
+
+    ;Haskell way: pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
+    (testing "Composition"
+      (let [u (->List [#(* 2 %)])
+            v (->List [#(+ 10 %)])
+            w xs]                                           ;just re-using same functor with other name to match haskell way above
+        (is (= (-> (pure List #(partial comp %))
+                   (<*> u)
+                   (<*> v)
+                   (<*> w))
+               (<*> u (<*> v w))))))
+
+    ;Homomorphism
+    ;The result of applying the pure value of f to the pure value of x
+    ; should be the same as applying f directly to x and then feeding that into pure.
+
+    ;Haskell way: pure f <*> pure x = pure (f x)
+    (testing "Homomorphism"
+      (let [f #(* 2 %) x 10]
+        ;Original example favors thread first macro -> but I think that
+        ;in this case this explicit version makes more clear what is going on
+        (is (= (<*> (pure List f) (pure List x))
+               (pure List (f x))))))))
