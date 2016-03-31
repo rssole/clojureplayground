0a0c5c6b62f7c15d9d53df32b9ca34cb487a2ad9
Rastko Soskic <rastko.soskic@gtech.com>
2016-03-31 17:42:43 +0200

    Codingame - APU: Init Phase solved

--- src/clojureplayground/codingame_helper.clj
+++ src/clojureplayground/codingame_helper.clj
@@ -10,30 +10,30 @@
 (defn allowed-lines [theta]
   (first
     (for [t [[#(<= % (/ Math/PI 8)) [(co 1 0 "E") (co 1 0 "E") (co -1 0 "W") (co -1 0 "W")]]
-           [#(and (> % (/ Math/PI 8)) (<= % (* Math/PI 0.375))) [(co 1 1 "SE") (co 1 -1 "NE") (co -1 1 "SW") (co -1 -1 "NW")]]
-           [#(and (> % (* Math/PI 0.375)) (<= % (/ Math/PI 2))) [(co 0 1 "S") (co 0 -1 "N") (co 0 1 "S") (co 0 -1 "N")]]]
-        :when ((first t) theta)] (second t))))
+             [#(and (> % (/ Math/PI 8)) (<= % (* Math/PI 0.375))) [(co 1 1 "SE") (co 1 -1 "NE") (co -1 1 "SW") (co -1 -1 "NW")]]
+             [#(and (> % (* Math/PI 0.375)) (<= % (/ Math/PI 2))) [(co 0 1 "S") (co 0 -1 "N") (co 0 1 "S") (co 0 -1 "N")]]]
+          :when ((first t) theta)] (second t))))
 
 (defn thor-move2 [adj opp theta]
   ((allowed-lines theta) (direction adj opp)))
 
 ;APU: Init Phase
 (let [inp ["00" "0."]
-      node? #(when (= \0 (.charAt %2 %)) %)
-      nright #(let [x (.indexOf % %2)]
-               (if (>= x 0) [x %3] [-1 -1]))
-      nbottom #()]
-  (map-indexed (fn [y row]
-                 (map-indexed (fn [x c]
-                                (when (node? c x)
-                                  (let [ntb (nbottom (drop (inc y) inp))
-                                        ntr (nright row (inc x) y)]))) row)) inp))
-
-
-
-
-
-
-
+      neighr (fn [x y row]
+               (let [nx (.indexOf row "0" (inc x))]
+                 (if (>= nx 0) [nx y] [-1 -1])))
+      neighb (fn [x y in]
+               (reduce (fn [[_ _ cnt] i]
+                         (if (= \0 (.charAt i x))
+                           (reduced [x cnt])
+                           [-1 -1 (inc cnt)])) [-1 -1 (inc y)] (drop (inc y) in)))]
+  (map #(clojure.string/join " " %)
+       (filter some?
+               (apply concat
+                      (map-indexed (fn [y row]
+                                     (map-indexed (fn [x c]
+                                                    (when (= c \0)
+                                                      (let [[x1 y1] (neighr x y row)
+                                                            [x2 y2] (neighb x y inp)] [x y x1 y1 x2 y2]))) row)) inp)))))
 
 
