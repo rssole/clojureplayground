205b9cca4aca7da6d6218c96ee4e917217861e82
Rastko Soskic <rastko.soskic@gtech.com>
2016-04-04 18:16:43 +0200

    I misread task, there are indices of exit gateways

--- src/clojureplayground/codingame_helper.clj
+++ src/clojureplayground/codingame_helper.clj
@@ -1,4 +1,5 @@
-(ns clojureplayground.codingame-helper)
+(ns clojureplayground.codingame-helper
+  (:gen-class))
 
 (defn direction [adj opp]
   (if (neg? adj)
@@ -18,22 +19,49 @@
   ((allowed-lines theta) (direction adj opp)))
 
 ;APU: Init Phase
-(let [inp ["00" "0."]
-      neighr (fn [x y row]
-               (let [nx (.indexOf row "0" (inc x))]
-                 (if (>= nx 0) [nx y] [-1 -1])))
-      neighb (fn [x y in]
-               (reduce (fn [[_ _ cnt] i]
-                         (if (= \0 (.charAt i x))
-                           (reduced [x cnt])
-                           [-1 -1 (inc cnt)])) [-1 -1 (inc y)] (drop (inc y) in)))]
-  (map #(clojure.string/join " " %)
-       (filter some?
-               (apply concat
-                      (map-indexed (fn [y row]
-                                     (map-indexed (fn [x c]
-                                                    (when (= c \0)
-                                                      (let [[x1 y1] (neighr x y row)
-                                                            [x2 y2] (neighb x y inp)] [x y x1 y1 x2 y2]))) row)) inp)))))
+#_(let [inp ["00" "0."]
+        neighr (fn [x y row]
+                 (let [nx (.indexOf row "0" (inc x))]
+                   (if (>= nx 0) [nx y] [-1 -1])))
+        neighb (fn [x y in]
+                 (reduce (fn [[_ _ cnt] i]
+                           (if (= \0 (.charAt i x))
+                             (reduced [x cnt])
+                             [-1 -1 (inc cnt)])) [-1 -1 (inc y)] (drop (inc y) in)))]
+    (map #(clojure.string/join " " %)
+         (filter some?
+                 (apply concat
+                        (map-indexed (fn [y row]
+                                       (map-indexed (fn [x c]
+                                                      (when (= c \0)
+                                                        (let [[x1 y1] (neighr x y row)
+                                                              [x2 y2] (neighb x y inp)] [x y x1 y1 x2 y2]))) row)) inp)))))
+
+; Auto-generated code below aims at helping you parse
+; the standard input according to the problem statement.
+
+(def s->i #(Integer/parseInt %))
+(def spws #(clojure.string/split % #"\s"))
+(defn node-to-cut [aloc links]
+  (if-let [fc (some #(let [{:keys [f t severed]} %]
+                      (when (and (= f aloc) (zero? t) (false? severed))
+                        %)) links)]
+    fc
+    (first (filter #(let [{:keys [f severed]} %]
+                     (and (= f aloc) (false? severed))) links))))
+
+(let [[_ L E] (map s->i (spws (read-line)))
+      [links egs] (split-at (* 2 L) (mapcat #(map s->i (spws %)) (repeatedly (+ E L) read-line)))
+      lks (vec
+            (map-indexed
+              #(let [[from to] %2]
+                {:i % :f from :t to :severed false})
+              (partition 2 links)))]
+  (println lks)
+  (loop [lks0 lks]
+    (let [SI (read) {:keys [i f t] :as fnd} (node-to-cut SI lks0)]
+      (println f t)
+      (recur (update-in lks0 [i :severed] #(identity %2) true)))))
 
 
+;(println (mapcat #(clojure.string/split % #"\s") (repeatedly 3 read-line)))
