cee7db05444355fe60bf5d64f5334a8713bb63e2
Rastko Soskic <rastko.soskic@gtech.com>
2016-10-28 12:22:02 +0200

    Dejo Josifovic's example improved

---
+++ src/clojureplayground/prime_numbers_fun.clj
@@ -0,0 +1,15 @@
+(ns amazon.prime-numbers-fun)
+
+(def is-prime?
+  (memoize
+    (fn [^Number num] ^Boolean
+    (cond
+      (<= num 1) false
+      (<= num 3) true
+      (or (zero? (mod num 2)) (zero? (mod num 3))) false
+      :else (reduce #(if (<= (* %2 %2) num)
+                      (reduced true)
+                      (if (or (zero? (mod num %2)) (zero? (mod num (+ %2 2))))
+                        (reduced false) %))
+                    true
+                    (range 1 (inc num)))))))
