3b94c1fb06ee54ae331edfa01d07f3e6fd55cd51
Rastko Soskic <r.soskic.ext@unycom.com>
2017-12-22 16:55:05 +0100

    Upgrading deps

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/core.async "0.2.374"]]
-  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
+                 [org.clojure/core.async "0.3.465"]]
+  :profiles {:dev {:dependencies [[midje "1.9.1"]]
                    :plugins [[lein-midje "3.2.1"]]}}
-  :plugins [[lein-cljfmt "0.3.0"]]
+  :plugins [[lein-cljfmt "0.5.7"]]
   :aliases {"clean-midje" ["do" ["clean"] ["compile"] ["midje"]]})
