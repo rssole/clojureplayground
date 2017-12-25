6a6135cfc58c46944355c8d9f95f9486760c58dd
Rastko Soskic <r.soskic.ext@unycom.com>
2017-12-25 16:47:14 +0100

    Day 1 AOC 2017.

--- src/adventofcode/mmxvii/solutions.clj
+++ src/adventofcode/mmxvii/solutions.clj
@@ -6,9 +6,17 @@
 (defn- crf [acc xs]
   (+ acc (* (ch->int (first xs)) (dec (count xs)))))
 
-(defn captcha [c]
+(defn- captcha-1st [c]                                      ;1st attempt
   (let [ps (partition-by identity c)
         rps (if (= (last c) (first c))
               (cons (concat (first ps) (last ps)) (rest (butlast ps)))
               ps)]
     (reduce crf 0 (filter #(> (count %) 1) rps))))
+
+(defn captcha [c]                                           ;Much simpler option after verifying with previous
+  (let [x (first c)
+        s (if (= x (last c)) (ch->int x) 0)]
+    (reduce + s
+            (map #(ch->int (first %))
+                 (filter (fn [[a b]] (= a b))
+                         (partition 2 1 c))))))
